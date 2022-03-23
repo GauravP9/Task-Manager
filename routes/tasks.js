4b2879f123170cee327c6697102e68aedd213a29
Gaurav Patil <92083568+xCaliber-1@users.noreply.github.com>
2022-03-23 20:10:28 +0530

    Update tasks.js

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,1 +1,12 @@
+const express = require('express')
+const router = express.Router()
 
+const {getAllTask, createTask,getSingleTask,updateTask,deleteTask} = require('../controllers/tasks')   //Making seperate directories because this will make easy & modular
+
+
+router.route('/').get(getAllTask).post(createTask)
+router.route('/:id').get(getSingleTask).patch(updateTask).delete(deleteTask)  //patch used for update
+
+
+
+module.exports = router
